792412587a1b0ecc5544484d6cd4bfc79e863583
Ricardo <eehaha@gmail.com>
2019-03-01 07:38:48 +0800

    Initial commit

---
+++ Cat Years/README.md
@@ -0,0 +1,4 @@
+A Simple iOS application that calculate the age of a cat
+
+It demo the use of ImageView, UIButton, UILabel, UITextInput.
+
--- Cat Years/ViewController.swift
+++ Cat Years/ViewController.swift
@@ -9,7 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var ageTextField: UITextField!
+    
+    @IBAction func getAge(_ sender: Any) {
+        if let age = ageTextField.text{
+            if let ageNumber  = Int(age){
+            let ageInCatYear = ageNumber * 7
+                resultLabel.text = "Your cat is " + String(ageInCatYear) + " in cat years"
+            }
+        }
+    }
+    @IBOutlet weak var resultLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
